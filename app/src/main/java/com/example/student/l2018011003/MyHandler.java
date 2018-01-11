446eb13906eb154cb867d50d119e80489b060797
ericchiu <ericjack7@gmail.com>
2018-01-11 14:26:42 +0800

    增加抓取description

--- app/src/main/java/com/example/student/l2018011003/MyHandler.java
+++ app/src/main/java/com/example/student/l2018011003/MyHandler.java
@@ -16,9 +16,8 @@ public class MyHandler extends DefaultHandler {
     boolean isTitle = false ;
     boolean isitem = false ;
     boolean isLink =false ;
+    boolean isDescription =false;
     StringBuilder SBLink = new StringBuilder();
-    //public ArrayList<String> titles = new ArrayList<>();
-    //public ArrayList<String> links = new ArrayList<>();
 
     public ArrayList<Moblie01NewsItem> newsItems = new ArrayList<>();
     Moblie01NewsItem item;
@@ -26,44 +25,48 @@ public class MyHandler extends DefaultHandler {
     @Override
     public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
         super.startElement(uri, localName, qName, attributes);
-        if(qName.equals("title"))
+        switch (qName)
         {
-            isTitle=true;
-        }
-        if (qName.equals("item"))
-        {
-            isitem=true;
-            item = new Moblie01NewsItem();
-        }
-        if(qName.equals("link"))
-        {
-            isLink=true;
+            case "title":
+                isTitle=true;
+                break;
+            case "item":
+                isitem=true;
+                item=new Moblie01NewsItem();
+                break;
+            case "link":
+                isLink=true;
+                break;
+            case "description":
+                isDescription=true;
+                break;
         }
     }
 
     @Override
     public void endElement(String uri, String localName, String qName) throws SAXException {
         super.endElement(uri, localName, qName);
-        if(qName.equals("title"))
+        switch (qName)
         {
-            isTitle=false;
-        }
-        if (qName.equals("item"))
-        {
-            isitem=false;
-            newsItems.add(item);
-        }
-        if (qName.equals("link"))
-        {
-            isLink=false;
-            Log.d("link",SBLink.toString());
-            if(isitem)
-            {
-                //links.add(SBLink.toString());
-                item.url = SBLink.toString();
-                SBLink=new StringBuilder();
-            }
-
+            case "title":
+                isTitle=false;
+                break;
+            case "item":
+                isitem=false;
+                newsItems.add(item);
+                break;
+            case "link":
+                isLink=false;
+                Log.d("link",SBLink.toString());
+                if(isitem)
+                {
+                    item.url=SBLink.toString();
+                    SBLink=new StringBuilder();
+                }
+                break;
+            case "description":
+                isDescription=false;
+                break;
         }
     }
 
@@ -73,12 +76,16 @@ public class MyHandler extends DefaultHandler {
         if(isTitle && isitem)
         {
             Log.d("Net",new String(ch,start,length));
-            //title.add(new String(ch,start,length));
             item.title=new String(ch,start,length);
         }
         if (isLink && isitem)
         {
            SBLink.append(new String(ch,start,length));
+        }
+        if(isDescription && isitem)
+        {
+            Log.d("description",new String(ch,start,length));
+            item.description=new String(ch,start,length);
         }
 
     }
